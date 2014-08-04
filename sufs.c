0e48d4c36d6882954f39ec8eb665a6230ddacdaa
Robin Schroer <sulamiification@gmail.com>
2014-08-04 14:39:55 +0200

    actually (un)register a filesystem with the kernel

--- sufs.c
+++ sufs.c
@@ -8,18 +8,36 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
 #include "sufs.h"
 
+struct file_system_type sufs_fs_type = {
+	.owner = THIS_MODULE,
+	.name = "sufs",
+};
+
 static int sufs_init()
 {
-	pr_info("SuFS loaded\n");
+	int retval;
+
+	retval = register_filesystem(&sufs_fs_type);
+	if (unlikely(retval))
+		pr_err("SuFS: Failed to register filesystem: %d\n", retval);
+	else
+		pr_info("SuFS: Module loaded\n");
 	return 0;
 }
 
 static void sufs_exit()
 {
-	pr_info("SuFS unloaded\n");
+	int retval;
+
+	retval = unregister_filesystem(&sufs_fs_type);
+	if (unlikely(retval))
+		pr_err("SuFS: Failed to unregister filesystem: %d\n", retval);
+	else
+		pr_info("SuFS: Module unloaded\n");
 }
 
 module_init(sufs_init);
