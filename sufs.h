412b9f82366d65b484860fc48c81528bb9f27c78
Robin Schroer <sulamiification@gmail.com>
2014-09-23 19:09:07 +0200

    add btree implementation from my spielwiese

--- sufs.h
+++ sufs.h
@@ -1,21 +1,21 @@
-/*
- * Header for SuFS
- */
+#include "sufs_btree.h"
 
 /* Magic number */
 #define SUFS_MAGIC_NR 0x5c1a31
 
 /* SuFS version, for compability checks */
-#define SUFS_VERSION 1;
+#define SUFS_VERSION 2;
 
 /* Default block size */
 const unsigned int SUFS_DEFAULT_BLOCK_SIZE = (4 * 1024);
 
+/* Super block layout */
 struct sufs_super_block {
 	unsigned int version;
 	unsigned int magic;
 	unsigned int block_size;
 	unsigned int free_blocks;
+	struct node *root_node;
 	char padding[(4 * 1024) - (4 * sizeof(unsigned int))];
 };
 
